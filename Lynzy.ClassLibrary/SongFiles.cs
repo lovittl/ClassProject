a70b080c81b4e27b8e175032c4aaf4385dd02550
Roy Lovitt <lovittl@my.erau.edu>
2018-05-15 11:50:27 -0400

    ,

--- Lynzy.ClassLibrary/SongFiles.cs
+++ Lynzy.ClassLibrary/SongFiles.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lynzy.ClassLibrary
 {
     class SongFiles
     {
+        
+        public string Name { get; set; }
+        public string Title { get; set; }
+        [Column("Contributing Artists")]
+        public string ContributingArtists { get; set; }
+        public string  Album { get; set; }
+        [Column("File Extension")]
+        public string FileExtension { get; set; }
+        public string Genre { get; set; }
+        public TimeSpan Length { get; set; }
+        public byte Favorite { get; set; }
+        public DateTime Year { get; set; }
     }
 }
