287afb236d0ca8cab394ac2aed34078310268100
Roy Lovitt <lovittl@my.erau.edu>
2018-05-15 14:44:17 -0400

    Added properties to SongFiles.cs

--- Lynzy.ClassLibrary/SongFiles.cs
+++ Lynzy.ClassLibrary/SongFiles.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 
 namespace Lynzy.ClassLibrary
 {
     class SongFiles
     {
-        
         public string Name { get; set; }
         public string Title { get; set; }
         [Column("Contributing Artists")]
